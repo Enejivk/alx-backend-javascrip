8939b9871f204ead6a40a3fd8e75bf7fa7385c0a
enejivk <enejivic@gmail.com>
2024-07-30 19:31:50 +0100

    function named cleanSet that returns a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const cleanSet = (set, sString) => {
-  if (!sString || sString.length < 0) {
-    return ' ';
-  }
-  const unMatchString = [];
-  for (const value of set) {
-    if (value.startsWith(sString)) {
-      unMatchString.push(value.slice(sString.length));
+    if (!sString || sString.length < 0) {
+        return '';
     }
-  }
-  return unMatchString.join('-');
+    const unMatchString = [];
+    for (const value of set) {
+        if (value.startsWith(sString)) {
+            unMatchString.push(value.slice(sString.length));
+        }
+    }
+    return unMatchString.join('-');
 };
-export default cleanSet;
+exports.default = cleanSet;
