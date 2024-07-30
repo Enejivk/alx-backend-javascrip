1afce4170ba05e7e7919e6ad8f98d2edb6f90be3
enejivk <enejivic@gmail.com>
2024-07-30 06:25:30 +0100

    correction for the first numberf

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+const getStudentIdsSum = (arrayList) => {
+  const sum = arrayList.reduce((previousValue, currentValue) => previousValue + currentValue.id, 0);
+  return sum;
+}
+export default getStudentIdsSum
---
+++ 0x03-ES6_data_manipulation/main.js
@@ -0,0 +1,5 @@
+const arr = ['a', 'b', 'd', 't', 'k', 'l']
+
+for(let i = 0; i < arr.length; i++){
+  console.log(arr[i] = "a" );
+}
